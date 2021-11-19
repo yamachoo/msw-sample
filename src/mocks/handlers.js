95ba0e59e857fccd3114495a836adff5ca47dd54
yamachoo <51016095+yamachoo@users.noreply.github.com>
2021-11-19 23:29:02 +0900

    add Response resolver

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,31 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.post('/login', null),
-  rest.get('/user', null)
+  rest.post('/login', (req, res, ctx) => {
+    sessionStorage.setItem('is-authenticated', 'true')
+
+    return res(
+      ctx.status(200)
+    )
+  }),
+
+  rest.get('/user', (req, res, ctx) => {
+    const isAuthenticated = sessionStorage.getItem('is-authenticated')
+
+    if (!isAuthenticated) {
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: 'Not authorized',
+        }),
+      )
+    }
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        username: 'admin',
+      })
+    )
+  })
 ]
